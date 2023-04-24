24f6870201e082bcceb8e20f4f694289d3216a56
kenney-84 <tkenney930w@gmail.com>
2023-04-24 12:39:21 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="./css/main.css">
+        <link rel="icon" type="image/x-icon" href="./images/favicon.ico">
+    </head>
+    <body class="black-background">
+        <h1 class="green-text">Test = <?php echo 10 * 4 ?></h1>
+    </body>
+</html>
