21d0c8354bdc59ad0274f0d22d6afc33bd51df03
Tyler J. Kenney <tkenney930w@gmail.com>
2023-04-24 15:05:29 -0400

    Website Backend Prep 1

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <link rel="stylesheet" href="./css/main.css">
-        <link rel="icon" type="image/x-icon" href="./images/favicon.ico">
+        <link rel="icon" type="image/x-icon" href="./media/favicon.ico">
     </head>
     <body class="black-background">
         <h1 class="green-text">Crappy Test = <?php echo 10 * 4 ?></h1>
