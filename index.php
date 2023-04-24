6e9a7394e8ccf19d652bbd171f132b16ef081e24
Tyler J. Kenney <tkenney930w@gmail.com>
2023-04-24 15:08:57 -0400

    Website Backend Prep 1

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
     </head>
     <body class="black-background">
         <h1 class="green-text">Crappy Test = <?php echo 10 * 4 ?></h1>
-        <?php include('pages/placeholder.html') ?>
+        <?php //include('pages/placeholder.html') ?>
     </body>
 </html>
