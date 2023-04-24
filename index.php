0bc6dead9dde2c276cd22a4d3d3a9483a46ebadb
Tyler J. Kenney <tkenney930w@gmail.com>
2023-04-24 14:29:00 -0400

    Website Backend Prep

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
     </head>
     <body class="black-background">
         <h1 class="green-text">Best Test = <?php echo 10 * 4 ?></h1>
+        <?php include('pages/placeholder.html') ?>
     </body>
 </html>
