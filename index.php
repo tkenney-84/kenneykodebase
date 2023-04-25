06dd2da44e2d31823ce249b6813e5918d541514e
kenney-84 <tkenney930w@gmail.com>
2023-04-25 12:30:20 -0400

    Backend Test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <link rel="icon" type="image/x-icon" href="./media/favicon.ico">
     </head>
     <body class="black-background">
-        <h1 class="green-text">Crappy Test = <?php echo 10 * 4 ?></h1>
-        <?php //include('pages/placeholder.html') ?>
+        <h1 class="green-text">Bad Test = <?php echo 10 * 4 / 10 ?></h1>
+        <?php include('pages/placeholder.html') ?>
     </body>
 </html>
